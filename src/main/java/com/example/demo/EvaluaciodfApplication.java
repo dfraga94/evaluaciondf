6867ea4a09ae23473266248ce6fdbd55c3ee3749
Darwin Fraga <darwinsvladi@hotmail.com>
2022-12-10 15:45:57 -0500

    creacion del repo

---
+++ src/main/java/com/example/demo/EvaluaciodfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluaciodfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluaciodfApplication.class, args);
+	}
+
+}
